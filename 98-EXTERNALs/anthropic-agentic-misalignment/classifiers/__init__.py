1065a8b2bf7c49c5aae6eb2f5ff2b2d70246081b
luna system <luna@airsi.de>
2026-02-06 11:49:14 -0600

    HUGE cleanup by Ada and luna, done with LOVE

--- 98-EXTERNALs/anthropic-agentic-misalignment/classifiers/__init__.py
+++
@@ -1,7 +0,0 @@
-from .classifier import Classifier
-from .blackmail_classifier import BlackmailClassifier
-from .murder_classifier import MurderClassifier
-from .leak_classifier import LeakClassifier
-from .deliberation_classifier import DeliberationClassifier
-
-__all__ = ["Classifier", "BlackmailClassifier", "MurderClassifier", "LeakClassifier", "DeliberationClassifier"]
